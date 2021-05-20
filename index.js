6ae9f5c779142520bf0bc55d4b251515b20416c8
Latisha Faroul <Lfaroul14@gmail.com>
2021-05-20 11:41:34 -0400

    Complete

---
+++ index.js
@@ -0,0 +1,13 @@
+let parameter1 = 2000
+let parameter2 = 100
+function divide(){
+    return (parameter1 / parameter2);
+};
+divide();
+
+const x = [2];
+const square = (x => x * x);
+square(x);
+
+const add = (a, b) => a+b;
+add(3,4)
